33c2e66e46a9693418ad97da30b40c1e7692f830
Vy Tran <vy.tran@alpaca.vn>
2021-10-05 20:23:09 +0700

    Commit Configuaration

---
+++ EcommerceWebsite.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using EcommerceWebsite.Data.Entities;
+using EcommerceWebsite.Data.Enum;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcommerceWebsite.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Status)
+                   .HasDefaultValue(Status.Active);
+        }
+    }
+}
