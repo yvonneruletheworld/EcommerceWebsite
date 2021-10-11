5026131863ccf67228ecdae6f1ddfc765c9c2e3d
Vy Tran <vy.tran@alpaca.vn>
2021-10-11 17:15:48 +0700

    design

---
+++ EcommerceWebsite.Application/Pagination/PageResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcommerceWebsite.Application.Pagination
+{
+    public class PageResponse<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+        public T Data { get; set; }
+
+        public PageResponse(T data, int pageNumber, int pageSize, int totalRecords)
+        {
+            int totalPage = totalRecords / pageSize;
+            this.TotalPages = (totalRecords % pageSize) != 0 ? totalPage + 1 : totalPage;
+            this.PageNumber = pageNumber;
+            this.PageSize = pageSize;
+            this.TotalRecords = totalRecords;
+            this.Data = data;
+        }
+    }
+}
---
+++ EcommerceWebsite.Services/Services/Main/ProductServices.cs
@@ -0,0 +1,46 @@
+﻿using EcommerceWebsite.Application.Pagination;
+using EcommerceWebsite.Data.EF;
+using EcommerceWebsite.Data.Entities;
+using EcommerceWebsite.Data.Identity;
+using EcommerceWebsite.Services.Interfaces.Main;
+using EcommerceWebsite.Utilities.Main;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceWebsite.Services.Services.Main
+{
+    public class ProductServices : IProductServices
+    {
+        private readonly EcomWebDbContext _context;
+        private readonly UserManager<KhachHangVM> _userManager;
+
+        public ProductServices(EcomWebDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IdentityResult> Create(KhachHangVM obj)
+        {
+            return await _userManager.CreateAsync(obj);
+        }
+
+        public List<Product> GetListProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PageResponse<List<ProductOutput>> GetListProductByPage(string key, int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? KiemTra(string value)
+        {
+            return value?.Contains("j");
+        }
+    }
+}
