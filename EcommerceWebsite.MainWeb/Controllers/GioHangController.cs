964912fa65cc57ebc1dbb2d07ba42579040663ae
Pham Thi Vi <69444848+PhamTVi@users.noreply.github.com>
2021-11-20 09:03:53 +0700

    3
    3

---
+++ EcommerceWebsite.MainWeb/Controllers/GioHangController.cs
@@ -0,0 +1,30 @@
+﻿using EcommerceWebsite.Utilities.Output.Main;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceWebsite.WebApp.Controllers.Main
+{
+    public class GioHangController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("/Views/GioHang/Index.cshtml");
+        }
+        ////lấy giỏ hàng
+        //public List<GioHangOutput> LayGioHang()
+        //{
+
+        //    List<GioHangOutput> listGHang = Session["cart"] as List<GioHangOutput>;
+        //    if (listGHang == null)
+        //    {
+        //        //Nếu gỏi k tồn tại thì khởi tạo
+        //        listGHang = new List<GioHangOutput>();
+        //        Session["cart"] = listGHang;
+        //    }
+        //    return listGHang;
+        //}
+    }
+}
